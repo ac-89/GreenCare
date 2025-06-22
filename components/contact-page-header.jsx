2fea9b88f46516c0836ae00cf8396f128146e9cd
ac-89 <alancarroll89@gmail.com>
2025-06-22 13:04:44 +0100

    Fix apostrophe error

--- components/contact-page-header.jsx
+++ components/contact-page-header.jsx
@@ -3,7 +3,7 @@ export default function ContactPageHeader() {
         <div className="contact-page-header">
             <img src="images/hero3.jpg" alt="" />
             <h1>Contact Us</h1>
-            <p>We'd love to hear from you!</p>
+            <p>We&#39;d love to hear from you!</p>
         </div>
     );
 }
